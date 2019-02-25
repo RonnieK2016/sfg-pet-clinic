60732673dcec2b3b4f91a5bbe26021e9207ae7c6
SergeA <ronnieo@gmail.com>
2019-02-25 16:52:04 -0500

    Added id to model classes. Closes #10

--- sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.udemy.sfg.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private String name;
--- sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.udemy.sfg.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     String typeName;
 
