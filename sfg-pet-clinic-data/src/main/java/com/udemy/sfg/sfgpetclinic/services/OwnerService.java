985480cc6eade6139105fefcbb5a3adb0bd5dfc3
SergeA <ronnieo@gmail.com>
2019-02-25 21:36:56 -0500

    Services refactored to have common CrudService. Closes #11

--- sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.udemy.sfg.sfgpetclinic.services;
 
 import com.udemy.sfg.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 }
