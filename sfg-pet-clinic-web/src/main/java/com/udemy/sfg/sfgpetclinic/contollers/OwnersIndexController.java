3d9d0017eacafa44d17aed165d6a82e98b494846
SergeA <ronnieo@gmail.com>
2019-03-02 23:07:54 -0500

    Added list of owners. Closes #16

--- sfg-pet-clinic-web/src/main/java/com/udemy/sfg/sfgpetclinic/contollers/OwnersIndexController.java
+++ sfg-pet-clinic-web/src/main/java/com/udemy/sfg/sfgpetclinic/contollers/OwnersIndexController.java
@@ -1,14 +1,23 @@
 package com.udemy.sfg.sfgpetclinic.contollers;
 
+import com.udemy.sfg.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersIndexController {
 
+    private OwnerService ownerService;
+
+    public OwnersIndexController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String ownersIndex() {
+    public String ownersIndex(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/ownerslist";
     }
 
