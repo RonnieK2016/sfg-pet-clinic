571fabb59c292bf157a3df0c08f08cfa07b8cc20
SergeA <ronnieo@gmail.com>
2019-03-09 12:56:52 -0500

    Create PetType Map Service Closes #29

---
+++ sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.udemy.sfg.sfgpetclinic.services;
+
+import com.udemy.sfg.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/services/map/impl/PetTypeMapService.java
@@ -0,0 +1,14 @@
+package com.udemy.sfg.sfgpetclinic.services.map.impl;
+
+import com.udemy.sfg.sfgpetclinic.model.PetType;
+import com.udemy.sfg.sfgpetclinic.services.PetTypeService;
+import com.udemy.sfg.sfgpetclinic.services.map.AbstractMapService;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long>
+        implements PetTypeService {
+
+    @Override
+    public void delete(PetType petType) {
+        super.deleteById(petType.getId());
+    }
+}
