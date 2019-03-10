328c743ccf12cf9fbdcea69643e7d98da642af51
SergeA <ronnieo@gmail.com>
2019-03-09 20:56:33 -0500

    Create Speciality Map Service Closes #31

---
+++ sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.udemy.sfg.sfgpetclinic.services;
+
+import com.udemy.sfg.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ sfg-pet-clinic-data/src/main/java/com/udemy/sfg/sfgpetclinic/services/map/impl/SpecialityMapService.java
@@ -0,0 +1,16 @@
+package com.udemy.sfg.sfgpetclinic.services.map.impl;
+
+import com.udemy.sfg.sfgpetclinic.model.Speciality;
+import com.udemy.sfg.sfgpetclinic.services.SpecialityService;
+import com.udemy.sfg.sfgpetclinic.services.map.AbstractMapService;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long>
+        implements SpecialityService {
+
+    @Override
+    public void delete(Speciality speciality) {
+        if(speciality != null) {
+            super.deleteById(speciality.getId());
+        }
+    }
+}
