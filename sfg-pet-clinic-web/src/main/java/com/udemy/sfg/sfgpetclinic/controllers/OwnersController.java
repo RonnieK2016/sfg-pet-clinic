e2ad32fcbe39679e3f8964b4841549ed4bfc3154
SergeA <ronnieo@gmail.com>
2019-03-14 12:11:13 -0400

    Display Owner Record (HTML). Closes #53

--- sfg-pet-clinic-web/src/main/java/com/udemy/sfg/sfgpetclinic/controllers/OwnersController.java
+++ sfg-pet-clinic-web/src/main/java/com/udemy/sfg/sfgpetclinic/controllers/OwnersController.java
@@ -3,27 +3,37 @@ package com.udemy.sfg.sfgpetclinic.controllers;
 import com.udemy.sfg.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
-public class OwnersIndexController {
+public class OwnersController {
 
     private OwnerService ownerService;
 
-    public OwnersIndexController(OwnerService ownerService) {
+    public OwnersController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String ownersIndex(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/ownerslist";
     }
 
-    @RequestMapping("/find")
+    @GetMapping("/find")
     public String ownersFind() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 
 }
--- sfg-pet-clinic-web/src/test/java/com/udemy/sfg/sfgpetclinic/controllers/OwnersControllerTest.java
+++ sfg-pet-clinic-web/src/test/java/com/udemy/sfg/sfgpetclinic/controllers/OwnersControllerTest.java
@@ -14,21 +14,21 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
-class OwnersIndexControllerTest {
+class OwnersControllerTest {
 
     @Mock
     OwnerService ownerService;
 
     @InjectMocks
-    OwnersIndexController ownersIndexController;
+    OwnersController ownersIndexController;
 
     MockMvc mockMvc;
 
@@ -59,5 +59,15 @@ class OwnersIndexControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
         verifyZeroInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
     }
 }
