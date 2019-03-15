8a228af62a3f64b8726bf364f92376bc31449dac
SergeA <ronnieo@gmail.com>
2019-03-15 15:41:07 -0400

    Implement Vets JSON Endpoint. Closes #66

--- sfg-pet-clinic-web/src/main/java/com/udemy/sfg/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic-web/src/main/java/com/udemy/sfg/sfgpetclinic/controllers/VetController.java
@@ -1,23 +1,32 @@
 package com.udemy.sfg.sfgpetclinic.controllers;
 
+import com.udemy.sfg.sfgpetclinic.model.Vet;
 import com.udemy.sfg.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
-@RequestMapping("/vets")
-public class VetsIndexController {
+public class VetController {
 
     private VetService vetService;
 
-    public VetsIndexController(VetService vetService) {
+    public VetController(VetService vetService) {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets/", "/vets/index", "vets.html"})
     public String vetsIndex(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/vetslist";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
